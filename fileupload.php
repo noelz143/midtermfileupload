ab6c5d7403be4a8e869649dbfb45735dbd10804f
noelz143 <57779619+noelz143@users.noreply.github.com>
2020-01-23 18:25:07 -0800

    Add files via upload

---
+++ fileupload.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>uploading</title>
+</head>
+<body>
+
+<form action="uploadserver.php" method="post" enctype="multipart/form-data">
+<label>Upload An Image</label>
+<input type="file" name="file_upload" />
+<input type="submit" name="upload"/>
+</form>
+
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>index</title>
+</head>
+<body>
+<a href="fileupload.php">
+	<button>click to upload file</button>
+</a>
+
+</body>
+</html>
